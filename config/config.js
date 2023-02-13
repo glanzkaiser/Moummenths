82cc8517c425777ee21a50b2bb05035755d05ea6
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2023-02-13 13:32:04 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    'mongodb+srv://glanzkaiser:GlanzFreya18825197~@cluster0.2eqmebe.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://glanzkaiser:<password>@cluster0.2eqmebe.mongodb.net/?retryWrites=true&w=majority'
 
     //'mongodb://' + (process.env.IP || 'localhost') + ':' +
     //(process.env.MONGO_PORT || '27017') +
